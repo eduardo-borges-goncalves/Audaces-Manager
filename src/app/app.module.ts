d1d0cfd70e8d2bf0c9cbfbbd38d2cf9f1ae53913
unknown <alemaobrk1@gmail.com>
2022-05-18 22:00:02 -0300

    cria rotas e implementa layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderDashboardComponent } from './header-dashboard/header-dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { ColectionsComponent } from './colections/colections.component';
+import { ModelsComponent } from './models/models.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    HeaderDashboardComponent,
+    DashboardComponent,
+    ColectionsComponent,
+    ModelsComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
