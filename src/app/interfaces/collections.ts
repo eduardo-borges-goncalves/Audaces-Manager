6a65db00df95459747b1caea298c342aa5b2eca0
unknown <alemaobrk1@gmail.com>
2022-05-20 21:49:58 -0300

    #8 busca coleções na API

---
+++ src/app/interfaces/collections.ts
@@ -0,0 +1,6 @@
+export interface Collection {
+  id: string, 
+  name: string, 
+  budget: string, 
+  responsible: string
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Collection } from 'src/app/interfaces/collections';
+import { CollectionsService } from 'src/app/services/collections/collections.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  collections:Collection[] = []
 
-  constructor() { }
+  constructor(private service: CollectionsService) { 
+    this.getCollections()
+  }
 
   ngOnInit(): void {
   }
 
+  getCollections() {
+    this.service.get().subscribe(collections => this.collections = collections)
+  }
 }
