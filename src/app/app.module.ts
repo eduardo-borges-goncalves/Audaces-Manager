c83b0b55230c5c1c6a904622812bc24b496bb7c6
unknown <alemaobrk1@gmail.com>
2022-05-22 23:19:08 -0300

    #10 configura bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http"
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './pages/home-page/home.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HeaderDashboardComponent } from './components/header-dashboard/header-dashboard.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ModelsComponent } from './pages/models/models.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { LoginComponent } from './pages/login-page/login.component';
+import { ForgotPasswordComponent } from './pages/forgot-password-page/forgot-password.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { CollectionsComponent } from './pages/collections/collections.component';
-import { EditModelComponent } from './pages/edit-model/edit-model.component';
-import { EditCollectionComponent } from './pages/edit-collection/edit-collection.component';
+import { CollectionsComponent } from './pages/collections-page/collections.component';
+import { EditModelComponent } from './pages/edit-model-page/edit-model.component';
+import { EditCollectionComponent } from './pages/edit-collection-page/edit-collection.component';
 import { CollectionsListComponent } from './components/collections-list/collections-list.component';
 import { ModelsListComponent } from './components/models-list/models-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { ModelsListComponent } from './components/models-list/models-list.compon
     BrowserModule,
     AppRoutingModule, 
     HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
