ae36094e9d320821ac4fdf5761759f55fb0f625c
Eduardo Borges <alemaobrk1@gmail.com>
2022-05-28 16:57:41 -0300

    #19 implementa CRUD collection

--- src/app/pages/edit-collection-page/edit-collection.component.ts
+++ src/app/pages/edit-collection-page/edit-collection.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Collection } from 'src/app/interfaces/collections';
+import { CollectionsService } from 'src/app/services/collections/collections.service';
 
 @Component({
   selector: 'app-edit-collection',
@@ -6,10 +10,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-collection.component.scss']
 })
 export class EditCollectionComponent implements OnInit {
+  public form!: FormGroup
+  public submitted = false
 
-  constructor() { }
+  constructor(
+    private _service: CollectionsService, 
+    private _fb: FormBuilder, 
+    private _router: Router,
+    private _route: ActivatedRoute
+    ) {}
 
   ngOnInit(): void {
+    let register = null
+
+    this.form = this._fb.group({
+      id: null, 
+      name: ["", [Validators.required, Validators.minLength(3)]], 
+      season: ["", [Validators.required, Validators.minLength(3)] ], 
+      budget: ["", [Validators.required, Validators.minLength(3)]], 
+      responsible: ["",[ Validators.required, Validators.minLength(3)]], 
+      brand: ["", [Validators.required, Validators.minLength(3)]],
+      launchYear: ["", [Validators.required, Validators.minLength(3)]],
+    })
+
+    this._route.params.subscribe((params: any) => {
+      const id = params["id"]
+      const collection$ = this._service.getOne(id);
+      collection$.subscribe(collection => {
+        register = collection
+        this.updateForm(collection)
+      })
+    })
   }
 
+  public updateForm (collection: Collection) {
+    this.form.patchValue({
+      id: collection.id, 
+      name: collection.name, 
+      season: collection.season, 
+      budget: collection.budget, 
+      responsible: collection.responsible,
+      brand: collection.brand,
+      launchYear: collection.launchYear, 
+    })
+  }
+
+  public onSubmit() {
+    if(this.form.valid) {
+      if(this.form.value.id) {
+        this._service.update(this.form.value).subscribe()
+        this._router.navigate(["/collections"])
+      } else {
+        this._service.post(this.form.value).subscribe()
+      }
+    }
+  }
+  
+  public onDelete() {
+    this._service.delete(this.form.value.id).subscribe()
+    this._router.navigate(["/collections"])
+  }
 }
--- src/app/services/collections/collections.service.ts
+++ src/app/services/collections/collections.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Collection } from 'src/app/interfaces/collections';
 
 @Injectable({
@@ -13,5 +13,21 @@ export class CollectionsService {
 
   get():Observable<Collection[]> {
     return this.http.get<Collection[]>(this.url)
+  }
+
+  getOne(id: string):Observable<any> {
+    return this.http.get(`${this.url}/${id}`).pipe(take(1))
+  }
+
+  post(data: Collection): Observable<any> {
+    return this.http.post(this.url, data).pipe(take(1))
+  }
+
+  update(data: Collection): Observable<any> {
+    return this.http.patch(`${this.url}/${data.id}`, data).pipe(take(1))
+  }
+ 
+  delete(id: string): Observable<any> {
+    return this.http.delete(`${this.url}/${id}`).pipe(take(1))
   }
 }
